715f1e96c5a45b0a2f718147d0967038542e2e3e
Mohammad Tariq <mohdat@gmail.com>
2014-03-24 13:02:18 +0400

    changing port to available port
    Signed-off-by: Mohammad Tariq <mohdat@gmail.com>

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
+var port = process.env.PORT || 8000;
 var Interfake = require('interfake');
 var interfake = new Interfake();
 interfake.get('/whats-next').body({ next : 'more stuff '});
-interfake.listen(3000); // The server will listen on port 3000
+interfake.listen(port); // 
